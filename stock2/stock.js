4e3b105b900a1e01e2969adc4558f26407e2e500
blueteeth48 <ayuishii.blue+git@gmail.com>
2014-10-11 12:26:33 -0700

    select stock with buttons

--- stock2/stock.js
+++ stock2/stock.js
@@ -6,34 +6,35 @@ function init() {
     showGraph();
 };
 
+var svg;
+var today = new Date();
+var lastYearToday = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+var x = d3.time.scale()
+    .domain([lastYearToday, today])
+    .range([0, width]);
+
+var y = d3.scale
+    .linear()
+    .domain([0, 200])
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickSize(-height);
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickSize(-width);
+
+
 function showGraph() {
-    var today = new Date();
-    var lastYearToday = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
-    var parseDate = d3.time.format("%Y-%m-%d").parse;
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    var x = d3.time.scale()
-        .domain([lastYearToday, today])
-        .range([0, width]);
-
-    var y = d3.scale
-        .linear()
-        .domain([0, 200])
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-        .tickSize(-height);
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .tickSize(-width);
-
     /*
     var zoom = d3.behavior.zoom()
         .x(x)
@@ -42,7 +43,8 @@ function showGraph() {
         .on("zoom", zoomed);
     */
 
-    var svg = d3.select("body").append("svg")
+    svg = d3.select("#graph").append("svg")
+        .attr("class", "center-block")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -67,43 +69,53 @@ function showGraph() {
         .attr("width", width)
         .attr("height", height);
 
-    var line = d3.svg.line()
-        .x(function(d) {return x(d.Date)})
-        .y(function(d) {return y(d.Close)})
-        .interpolate("linear");
-
     function zoomed() {
         svg.select(".x.axis").call(xAxis);
         svg.select(".y.axis").call(yAxis);
         svg.select("path.line").attr("d", line);
     }
 
-    getStock({stock: 'VTI', startDate: formatDate(lastYearToday), endDate: formatDate(today)}, 'historicaldata', function(err, data) {
+}
+
+function getStockBySymbol(evt) {
+    var parseDate = d3.time.format("%Y-%m-%d").parse;
+    var symbol = evt.target.attributes.symbol.value;
+
+    getStockInfo({stock: symbol, startDate: formatDate(lastYearToday), endDate: formatDate(today)}, 'historicaldata', function(err, data) {
         var quotes = data.quote;
         quotes.reverse();
-
         quotes.forEach(function(d) {
             d.Date = parseDate(d.Date);
             d.Close =+ d.Close;
         });
-
-        y.domain(d3.extent(quotes, function(d) { return d.Close;}))
-        var svgTrans = d3.select("body").transition();
-        svgTrans.select(".y.axis").duration(750).call(yAxis);
-
-        var path = svg.append('path')
-            .datum(quotes)
-            .attr('class', 'line')
-            .attr('d', line)
-            .attr("clip-path", "url(#clip)");
-
-        var totalLength = path.node().getTotalLength();
-        path.attr("stroke-dasharray", totalLength + " " + totalLength)
-            .attr("stroke-dashoffset", totalLength)
-            .transition()
-            .duration(1500)
-            .attr("stroke-dashoffset", 0);
+        redrawGraphLine(quotes);
     });
+}
+
+function redrawGraphLine(quotes) {
+    $('path').remove();
+    var line = d3.svg.line()
+        .x(function(d) {return x(d.Date)})
+        .y(function(d) {return y(d.Close)})
+        .interpolate("linear");
+
+    y.domain(d3.extent(quotes, function(d) { return d.Close;}))
+
+    var svgTrans = d3.select("body").transition();
+    svgTrans.select(".y.axis").duration(750).call(yAxis);
+
+    var path = svg.append('path')
+        .datum(quotes)
+        .attr('class', 'line')
+        .attr('d', line)
+        .attr("clip-path", "url(#clip)");
+
+    var totalLength = path.node().getTotalLength();
+    path.attr("stroke-dasharray", totalLength + " " + totalLength)
+        .attr("stroke-dashoffset", totalLength)
+        .transition()
+        .duration(1500)
+        .attr("stroke-dashoffset", 0);
 }
 
 function formatDate(date) {
@@ -114,7 +126,7 @@ function formatDate(date) {
     ].join('-');
 }
 
-function getStock(opts, type, complete) {
+function getStockInfo(opts, type, complete) {
     var defs = {
         desc: false,
         baseURL: 'http://query.yahooapis.com/v1/public/yql?q=',
